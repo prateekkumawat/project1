f09a2ee46517bebc6ce89e5cc4944f05062b4cb8
kanikaagarwal1997 <kanika45.agarwal@gmail.com>
2018-07-12 19:00:07 +0530

    reconfig

--- deploymet_script.sh
+++ deploymet_script.sh
@@ -2,5 +2,5 @@ mkdir /test
 sudo git clone https://github.com/prateekkumawat/project1.git 
 sudo mkdir /home/dep-project && cd /home/dep-project
 cp -rvf /test/project1/*  /home/dep-project 
-sudo ansible-playbook project_dep.yml 
+sudo ansible-playbook ./project_dep.yml 
 echo "Succcesfull playbook run"
